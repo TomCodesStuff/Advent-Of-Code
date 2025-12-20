0fd6ed675b6adcb415ed2411843328d504c83d36
Tom <thomasgibson2003@live.com>
2025-12-20 22:34:37 +0000

    Refactor 2023 days 7 8 to work with Solution class
    Fixed any inalid escape warnings in regex expressions

--- 2023/code/day_7.py
+++ 2023/code/day_7.py
@@ -1,104 +1,110 @@
-if(__name__ == "__main__"):
-    from puzzle_input import getFilePath
-else:
-    from .puzzle_input import getFilePath
+from solution_base.solution import Solution 
 
-TYPES = 7
-SIZEOFHAND = 5
+# TODO move repeated code and break up line long lines 
 
-def parse(line):
-    return line.rstrip().split(" ")
+class DaySeven(Solution):  
+    def __init__(self):
+        self.types = 7
+        self.sizeOfHand = 5
 
-def insertList(hand, handLst, jokers):
-    pokercards = {
-        "A": 12, 
-        "K": 11, 
-        "Q": 10, 
-        "J": -1 if jokers else 9, 
-        "T": 8, 
-        "9": 7,
-        "8": 6, 
-        "7": 5, 
-        "6": 4, 
-        "5": 3, 
-        "4": 2, 
-        "3": 1, 
-        "2" : 0, 
-    }
-    for index, sortedHand in enumerate(handLst):
-        for i in range(SIZEOFHAND):
-            if(pokercards[hand[i]] < pokercards[sortedHand[0][i]]): return index
-            if(pokercards[hand[i]] > pokercards[sortedHand[0][i]]): break
-    return len(handLst)
 
-def calculateTotal(handStrengths):
-    total = 0
-    counter = 1
-    for level in handStrengths:
-        for hand in level:
-            total += int(hand[1]) * counter
-            counter += 1
-    return total
+    def insert_list(self, hand, handLst, jokers):
+        pokercards = {
+            "A": 12, 
+            "K": 11, 
+            "Q": 10, 
+            "J": -1 if jokers else 9, 
+            "T": 8, 
+            "9": 7,
+            "8": 6, 
+            "7": 5, 
+            "6": 4, 
+            "5": 3, 
+            "4": 2, 
+            "3": 1, 
+            "2" : 0, 
+        }
 
-def taskOne():
-    with open(getFilePath()) as file:
-        handStrengths = [[] for _ in range(TYPES)]
-        hands = [parse(line) for line in file]
-   
-    for hand in hands:
-        charOccurence = {}
-        for char in hand[0]:
-            if(char not in charOccurence):
-                charOccurence[char] = 1
+        for index, sortedHand in enumerate(handLst):
+            for i in range(self.sizeOfHand):
+                if(pokercards[hand[i]] < pokercards[sortedHand[0][i]]): return index
+                if(pokercards[hand[i]] > pokercards[sortedHand[0][i]]): break
+        return len(handLst)
+
+
+    def calculate_total(self, handStrengths):
+        total = 0
+        counter = 1
+        for level in handStrengths:
+            for hand in level:
+                total += int(hand[1]) * counter
+                counter += 1
+        return total
+
+
+    def task_one(self): 
+        puzzle_input = self.get_puzzle_input()
+        handStrengths = [[] for _ in range(self.types)]
+        hands = [line.split(" ") for line in puzzle_input]
+    
+        for hand in hands:
+            charOccurence = {}
+            for char in hand[0]:
+                if(char not in charOccurence):
+                    charOccurence[char] = 1
+                else:
+                    charOccurence[char] += 1
+            
+            pairs = 0
+            largestOccurence = 0
+            for char, occurence in charOccurence.items():
+                if(occurence == 2): pairs += 1
+                if(occurence > largestOccurence): largestOccurence = occurence  
+            if(largestOccurence <= 2):
+                handStrengths[pairs].insert(self.insert_list(hand[0], handStrengths[pairs], False), hand)
+            elif(largestOccurence == 3):
+                handStrengths[largestOccurence + pairs].insert(self.insert_list(hand[0], handStrengths[largestOccurence + pairs], False), hand)
             else:
-                charOccurence[char] += 1
-        
-        pairs = 0
-        largestOccurence = 0
-        for char, occurence in charOccurence.items():
-            if(occurence == 2): pairs += 1
-            if(occurence > largestOccurence): largestOccurence = occurence  
-        if(largestOccurence <= 2):
-            handStrengths[pairs].insert(insertList(hand[0], handStrengths[pairs], False), hand)
-        elif(largestOccurence == 3):
-            handStrengths[largestOccurence + pairs].insert(insertList(hand[0], handStrengths[largestOccurence + pairs], False), hand)
-        else:
-            handStrengths[largestOccurence + 1].insert(insertList(hand[0], handStrengths[largestOccurence + 1], False), hand)
-    return calculateTotal(handStrengths)
+                handStrengths[largestOccurence + 1].insert(self.insert_list(hand[0], handStrengths[largestOccurence + 1], False), hand)
+        return self.calculate_total(handStrengths)
 
-def taskTwo():
-    with open(getFilePath()) as file:
-        handStrengths = [[] for _ in range(TYPES)]
-        hands = [parse(line) for line in file]
-   
-    for hand in hands:
-        charOccurence = {}
-        jokers = 0
-        for char in hand[0]:
-            if(char == "J"): jokers+=1
-            elif(char not in charOccurence):
-                charOccurence[char] = 1
+
+    def task_two(self):
+        puzzle_input = self.get_puzzle_input()
+        handStrengths = [[] for _ in range(self.types)]
+        hands = [line.split(" ") for line in puzzle_input]
+    
+        for hand in hands:
+            charOccurence = {}
+            jokers = 0
+            for char in hand[0]:
+                if(char == "J"): jokers+=1
+                elif(char not in charOccurence):
+                    charOccurence[char] = 1
+                else:
+                    charOccurence[char] += 1 
+            
+            pairs = 0
+            largestOccurence = 0
+            for char, occurence in charOccurence.items():
+                if(occurence == 2): pairs += 1
+                if(occurence > largestOccurence): largestOccurence = occurence      
+            if(jokers == 1 and largestOccurence == 1):
+                pairs = 1
+            elif(jokers == 1 and largestOccurence == 2 and pairs > 0):
+                pairs -= 1
+            
+            largestOccurence += jokers
+            if(largestOccurence <= 2):
+                handStrengths[pairs].insert(self.insert_list(hand[0], handStrengths[pairs], True), hand)
+            elif(largestOccurence == 3):
+                handStrengths[largestOccurence + pairs].insert(self.insert_list(hand[0], handStrengths[largestOccurence + pairs], True), hand)
             else:
-                charOccurence[char] += 1 
-         
-        pairs = 0
-        largestOccurence = 0
-        for char, occurence in charOccurence.items():
-            if(occurence == 2): pairs += 1
-            if(occurence > largestOccurence): largestOccurence = occurence      
-        if(jokers == 1 and largestOccurence == 1):
-            pairs = 1
-        elif(jokers == 1 and largestOccurence == 2 and pairs > 0):
-            pairs -= 1
-        
-        largestOccurence += jokers
-        if(largestOccurence <= 2):
-            handStrengths[pairs].insert(insertList(hand[0], handStrengths[pairs], True), hand)
-        elif(largestOccurence == 3):
-            handStrengths[largestOccurence + pairs].insert(insertList(hand[0], handStrengths[largestOccurence + pairs], True), hand)
-        else:
-            handStrengths[largestOccurence + 1].insert(insertList(hand[0], handStrengths[largestOccurence + 1], True), hand)    
-    return calculateTotal(handStrengths)
+                handStrengths[largestOccurence + 1].insert(self.insert_list(hand[0], handStrengths[largestOccurence + 1], True), hand)    
+        return self.calculate_total(handStrengths)
 
-print(f"Part 1: {taskOne()}")
-print(f"Part 2: {taskTwo()}")
+
+if __name__ == "__main__": 
+    s = DaySeven() 
+    print(f"Part 1: {s.task_one()}")
+    print(f"Part 2: {s.task_two()}")
--- 2023/code/day_8.py
+++ 2023/code/day_8.py
@@ -1,9 +1,7 @@
 import re
 import math 
-if(__name__ == "__main__"):
-    from puzzle_input import getFilePath
-else:
-    from .puzzle_input import getFilePath
+from solution_base.solution import Solution 
+
 
 class Node():
     def __init__(self, val):
@@ -11,12 +9,15 @@ class Node():
         self.left = None
         self.right = None 
         
-def createNetwork(file):
-    nodeDict =  {}
-    startDict = {}
-    for line in file:
-        if(line != "\n"):
-            nodes = re.sub("\s=|[(),]", "", line.rstrip()).split(" ")
+   
+class DayEight(Solution): 
+
+
+    def create_network(self, puzzle_input):
+        nodeDict =  {}
+        startDict = {}
+        for line in puzzle_input:
+            nodes = re.sub(" =|[(),]", "", line.rstrip()).split(" ")
             if(nodes[0] not in nodeDict):
                 nodeDict[nodes[0]] = Node(nodes[0])
                 if(nodes[0][-1] == "A" and nodes[0] not in startDict):
@@ -25,39 +26,45 @@ def createNetwork(file):
             if(nodes[2] not in nodeDict): nodeDict[nodes[2]] = Node(nodes[2])
             nodeDict[nodes[0]].left = nodeDict[nodes[1]]
             nodeDict[nodes[0]].right = nodeDict[nodes[2]]
-    return startDict
+        return startDict
 
-def traverse(instructions, startNode):
-    currentNode = startNode
-    i = 0
-    n = len(instructions)
-    steps = 0
-    while(True):
-        if(currentNode.val[-1] == "Z"): break
-        if(currentNode.left.val == currentNode.val and currentNode.right.val == currentNode.val): break 
-        if(instructions[i] == "R"): currentNode = currentNode.right
-        if(instructions[i] == "L"): currentNode = currentNode.left
-        if(i == n - 1): i = 0
-        else: i += 1
-        steps += 1
-    return steps
 
-def taskOne():
-    with open(getFilePath()) as file:
-        instructions = [*file.readline().rstrip()]
-        startNode = createNetwork(file)
-    return traverse(instructions, startNode["AAA"])
+    def traverse(self, instructions, startNode):
+        currentNode = startNode
+        i = 0
+        n = len(instructions)
+        steps = 0
+        while(currentNode.val[-1] != "Z"):
+            if(currentNode.left.val == currentNode.val and currentNode.right.val == currentNode.val): break 
+            if(instructions[i] == "R"): currentNode = currentNode.right
+            if(instructions[i] == "L"): currentNode = currentNode.left
+            if(i == n - 1): i = 0
+            else: i += 1
+            steps += 1
+        return steps
 
-def taskTwo():
-    nearestEnd = []
-    with open(getFilePath()) as file:
-        instructions = [*file.readline().rstrip()]
-        nodes = createNetwork(file)
-        for _, node in nodes.items(): nearestEnd.append(traverse(instructions, node))
-    lcm = 1 
-    for num in nearestEnd:
-        lcm = math.lcm(lcm, num)
-    return lcm
 
-print(f"Part 1: {taskOne()}")
-print(f"Part 2: {taskTwo()}")
+    def task_one(self):
+        puzzle_input = self.get_puzzle_input()
+        instructions = [c for c in puzzle_input[0]]
+        startNode = self.create_network(puzzle_input[2:])
+        return self.traverse(instructions, startNode["AAA"])
+
+
+    def task_two(self):
+        puzzle_input = self.get_puzzle_input()
+        instructions = [c for c in puzzle_input[0]]
+        nodes = self.create_network(puzzle_input[2:]) 
+
+        nearestEnd = []
+        for _, node in nodes.items(): nearestEnd.append(self.traverse(instructions, node))
+        lcm = 1 
+        for num in nearestEnd:
+            lcm = math.lcm(lcm, num)
+        return lcm
+
+
+if __name__ == "__main__": 
+    s = DayEight() 
+    print(f"Part 1: {s.task_one()}")
+    print(f"Part 2: {s.task_two()}")
