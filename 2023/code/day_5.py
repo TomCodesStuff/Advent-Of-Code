58dff4e8481ddb748bebead96396500301b78b6e
Tom <thomasgibson2003@live.com>
2025-12-20 22:00:22 +0000

    Refactor 2023 day 5 to work with Solution class
    I have tried to preserve the original spirit of the solution as much
    as possible but it has been edited a somewhat

--- 2023/code/day_5.py
+++ 2023/code/day_5.py
@@ -1,112 +1,116 @@
-if(__name__ == "__main__"):
-    from puzzle_input import getFilePath
-else:
-    from .puzzle_input import getFilePath
+import math
 import re
+from solution_base.solution import Solution 
 
-def parseSeeds(line):
-    return re.sub("[a-zA-Z]+[:]+[\s]", "", line).split(" ") 
 
-def parse(filePtr):
-    spaceCount = 0
-    numLst = []
-    while(spaceCount < 1):
-        line = filePtr.readline()
-        if(line == "\n" or line == ""): spaceCount += 1
-        if(re.match("[0-9]", line)):
-            nums = line.rstrip().split(" ")
-            numLst.append([int(nums[1]), int(nums[1]) + int(nums[2]) - 1, int(nums[0]) - int(nums[1])]) 
-    return sorted(numLst)
+class DayFive(Solution): 
 
-def binSearch(lst, val):
-    low = 0 
-    high = len(lst) - 1
-    while(low <= high): 
-        mid = (low + high) // 2
-        if(val < lst[mid][0]): 
-            high = mid - 1
-        elif(val > lst[mid][1]):
-            low = mid + 1  
-        else: return mid 
-    return -1
 
-def mapVals(lst, mappedVal):
-    index = binSearch(lst, mappedVal)
-    if (index != -1): 
-            return mappedVal + lst[index][2] 
-    return mappedVal
+    def parse_seeds(self, line):
+        return re.sub("[a-zA-Z]+[:]+[ ]", "", line).split(" ") 
 
-def taskOne():
-    with open(getFilePath()) as file: 
-        seeds = parseSeeds(file.readline().rstrip()) 
-        file.readline()
-        seedsToSoil = parse(file)    
-        soilToFertilizer = parse(file)
-        fertToWater = parse(file)
-        waterToLight = parse(file)
-        lightToTemp = parse(file)
-        tempToHumid = parse(file)
-        humidToLoc = parse(file)
+
+    def parse_puzzle_input(self):
+        puzzle_input = self.get_puzzle_input()
+        seeds = self.parse_seeds(puzzle_input[0]) 
+
+        almanacRegex = "[0-9]+ [0-9]+ [0-9]+"
+        gardenMappings = [[]]
+        i = 0
+
+        for line in puzzle_input[2:]: 
+            mapping_match = re.match(almanacRegex, line)
+            if mapping_match: 
+                a, b, c = map(int, mapping_match.group().split(" "))
+                gardenMappings[i].append([b, b + c - 1, a - b])
+            elif line == "": 
+                gardenMappings[i].sort()
+                gardenMappings.append([])
+                i += 1 
         
-        lowestLocation = None 
+        return (seeds, gardenMappings)
+
+
+
+    def parse_map(filePtr):
+        spaceCount = 0
+        numLst = []
+        while(spaceCount < 1):
+            line = filePtr.readline()
+            if(line == "\n" or line == ""): spaceCount += 1
+            if(re.match("[0-9]", line)):
+                nums = line.rstrip().split(" ")
+                numLst.append([int(nums[1]), int(nums[1]) + int(nums[2]) - 1, int(nums[0]) - int(nums[1])]) 
+        return sorted(numLst)
+
+
+    def binary_search(self, lst, val):
+        low = 0 
+        high = len(lst) - 1
+        while(low <= high): 
+            mid = (low + high) // 2
+            if(val < lst[mid][0]): 
+                high = mid - 1
+            elif(val > lst[mid][1]):
+                low = mid + 1  
+            else: return mid 
+        return -1
+
+
+    def map_values(self, lst, mappedVal):
+        index = self.binary_search(lst, mappedVal)
+        if (index != -1): return mappedVal + lst[index][2] 
+        return mappedVal
+
+
+    def task_one(self):
+        seeds, gardenMappings = self.parse_puzzle_input()
+        lowestLocation = math.inf
         for seed in seeds:
-            mappedVal = int(seed)
-            mappedVal = mapVals(seedsToSoil, mappedVal)
-            mappedVal = mapVals(soilToFertilizer, mappedVal) 
-            mappedVal = mapVals(fertToWater, mappedVal) 
-            mappedVal = mapVals(waterToLight, mappedVal)  
-            mappedVal = mapVals(lightToTemp, mappedVal)  
-            mappedVal = mapVals(tempToHumid, mappedVal) 
-            mappedVal = mapVals(humidToLoc, mappedVal) 
-            if(lowestLocation == None or mappedVal < lowestLocation):  lowestLocation = mappedVal
+            mappedVal = int(seed) 
+            for mapping in gardenMappings:
+                mappedVal = self.map_values(mapping, mappedVal)     
+            lowestLocation = min(lowestLocation, mappedVal)      
         return lowestLocation
 
-def parseSeedRanges(line):
-    seeds = []
-    nums = re.sub("[a-zA-Z]+[:]+[\s]", "", line).split(" ") 
-    for i in range(0, len(nums) - 1, 2):
-        seeds.append([int(nums[i]), int(nums[i]) + int(nums[i + 1]) - 1]) 
-    return sorted(seeds)
 
-def splitRanges(splitLst, checkLst):
-    i = 0 
-    while(i < len(splitLst)):
-        for j in range(0, len(checkLst)):
-            if(splitLst[i][0] > checkLst[j][1]): continue
-            if(splitLst[i][1] < checkLst[j][0]): continue
-            if(splitLst[i][0] >= checkLst[j][0] and splitLst[i][1] > checkLst[j][1]): 
-                splitLst.append([checkLst[j][1] + 1, splitLst[i][1]])
-                splitLst[i][1] = checkLst[j][1]
-            elif(splitLst[i][0] < checkLst[j][0] and splitLst[i][1] <= checkLst[j][1]):
-                splitLst.append([splitLst[i][0], checkLst[j][0] - 1])
-                splitLst[i][0] = checkLst[j][0]
-            splitLst[i][0] += checkLst[j][2]
-            splitLst[i][1] += checkLst[j][2] 
-            break 
-        i+=1
-    return splitLst
+    def parse_seed_ranges(self, seeds):
+        seed_ranges = []
+        for i in range(0, len(seeds) - 1, 2): 
+            seed_ranges.append([int(seeds[i]), int(seeds[i]) + int(seeds[i + 1]) - 1])
+        return sorted(seed_ranges)
 
-def taskTwo():
-      with open(getFilePath()) as file: 
-        seeds = parseSeedRanges(file.readline().rstrip()) 
-        file.readline()
-        seedsToSoil = parse(file)    
-        soilToFertilizer = parse(file)
-        fertToWater = parse(file)
-        waterToLight = parse(file)
-        lightToTemp = parse(file)
-        tempToHumid = parse(file)
-        humidToLoc = parse(file)   
 
-        mappedVal = splitRanges(seeds, seedsToSoil)
-        mappedVal = splitRanges(mappedVal, soilToFertilizer)
-        mappedVal = splitRanges(mappedVal, fertToWater)   
-        mappedVal = splitRanges(mappedVal, waterToLight)
-        mappedVal = splitRanges(mappedVal, lightToTemp)
-        mappedVal = splitRanges(mappedVal, tempToHumid)
-        mappedVal = splitRanges(mappedVal, humidToLoc)
+    def split_ranges(self, splitLst, checkLst):
+        i = 0 
+        while(i < len(splitLst)):
+            for j in range(0, len(checkLst)):
+                if(splitLst[i][0] > checkLst[j][1]): continue
+                if(splitLst[i][1] < checkLst[j][0]): continue
+                if(splitLst[i][0] >= checkLst[j][0] and splitLst[i][1] > checkLst[j][1]): 
+                    splitLst.append([checkLst[j][1] + 1, splitLst[i][1]])
+                    splitLst[i][1] = checkLst[j][1]
+                elif(splitLst[i][0] < checkLst[j][0] and splitLst[i][1] <= checkLst[j][1]):
+                    splitLst.append([splitLst[i][0], checkLst[j][0] - 1])
+                    splitLst[i][0] = checkLst[j][0]
+                splitLst[i][0] += checkLst[j][2]
+                splitLst[i][1] += checkLst[j][2] 
+                break 
+            i+=1
+        return splitLst
+
+
+    def task_two(self): 
+        seeds, gardenMappings = self.parse_puzzle_input()  
+        seeds = self.parse_seed_ranges(seeds)
+        mappedVal = seeds
+        for mapping in gardenMappings: 
+            mappedVal = self.split_ranges(mappedVal, mapping)
         mappedVal.sort()
         return mappedVal[0][0]
 
-print(f"Part 1: {taskOne()}")
-print(f"Part 2: {taskTwo()}")
+
+if __name__ == "__main__": 
+    s = DayFive() 
+    print(f"Part 1: {s.task_one()}")
+    print(f"Part 2: {s.task_two()}")
